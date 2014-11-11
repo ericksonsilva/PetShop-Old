3d2f767f4264b7af9e4810fdf43e58680ce8dab3
Lais Soares <laisoaresilva@gmail.com>
2014-11-11 09:41:54 -0300

    Cria diretorio CSS e adiciona entidade Funcionario

---
+++ src/br/com/PetShopPlus/model/Funcionario.java
@@ -0,0 +1,15 @@
+package br.com.PetShopPlus.model;
+
+public class Funcionario {
+	
+	private String nome;
+	private String cpf;
+	private String telefone;
+	private String rua;
+	private String complemento;
+	private String bairro;
+	private String cidade;
+	private String funcao;
+	private String login;
+	private String senha;
+}
