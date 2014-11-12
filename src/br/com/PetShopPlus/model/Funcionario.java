9c7e87f512a3cda338f9c02438a6b9eca8861548
Erickson Silva <ericksonsilva@live.com>
2014-11-12 19:21:34 -0300

    Corrige Funcionario e .gitignore

--- src/br/com/PetShopPlus/model/Funcionario.java
+++ src/br/com/PetShopPlus/model/Funcionario.java
@@ -12,20 +12,6 @@ public class Funcionario {
 	private String cidade;
 	private String email;
 	private String funcao;
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-
-=======
->>>>>>> 1ec3a26025cf6bec99e14ac1347104702287bef7
-	
-	private String login;
-	private String senha;
-	
-<<<<<<< HEAD
-=======
->>>>>>> 82ef4f1058b761b1bbbea04c12e526eded0fbed2
->>>>>>> 1ec3a26025cf6bec99e14ac1347104702287bef7
 	
 	public String getNome() {
 		return nome;
@@ -39,23 +25,12 @@ public class Funcionario {
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
 	}
-<<<<<<< HEAD
-
-=======
-<<<<<<< HEAD
->>>>>>> 1ec3a26025cf6bec99e14ac1347104702287bef7
 	public String getRg() {
 		return rg;
 	}
 	public void setRg(String rg) {
 		this.rg = rg;
 	}
-<<<<<<< HEAD
-
-=======
-=======
->>>>>>> 82ef4f1058b761b1bbbea04c12e526eded0fbed2
->>>>>>> 1ec3a26025cf6bec99e14ac1347104702287bef7
 	public String getTelefone() {
 		return telefone;
 	}
@@ -86,51 +61,18 @@ public class Funcionario {
 	public void setCidade(String cidade) {
 		this.cidade = cidade;
 	}
-<<<<<<< HEAD
 
-=======
-<<<<<<< HEAD
->>>>>>> 1ec3a26025cf6bec99e14ac1347104702287bef7
 	public String getEmail() {
 		return email;
 	}
 	public void setEmail(String email) {
 		this.email = email;
 	}
-<<<<<<< HEAD
 
-=======
-=======
->>>>>>> 82ef4f1058b761b1bbbea04c12e526eded0fbed2
->>>>>>> 1ec3a26025cf6bec99e14ac1347104702287bef7
 	public String getFuncao() {
 		return funcao;
 	}
 	public void setFuncao(String funcao) {
 		this.funcao = funcao;
 	}
-<<<<<<< HEAD
-
-=======
-<<<<<<< HEAD
-=======
->>>>>>> 1ec3a26025cf6bec99e14ac1347104702287bef7
-	public String getLogin() {
-		return login;
-	}
-	public void setLogin(String login) {
-		this.login = login;
-	}
-	public String getSenha() {
-		return senha;
-	}
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
-	
-<<<<<<< HEAD
-=======
-	
->>>>>>> 82ef4f1058b761b1bbbea04c12e526eded0fbed2
->>>>>>> 1ec3a26025cf6bec99e14ac1347104702287bef7
 }
